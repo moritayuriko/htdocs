8c878e6172536a50d7b1417075eb1f7e6a1d03a0
moritayuriko <yuriko-morita@infinityloop.co.jp>
2024-05-27 17:29:44 +0900

    4-10

---
+++ sa4-5.php
@@ -0,0 +1,16 @@
+<?php
+$num =0;
+echo "繰り返し回数：{$num}<br>:";
+echo "whileループによる処理";
+$i=0;
+while($i<$num){
+echo "*";
+$i++;
+}
+echo"<br>do~whileによるループ:";
+$i = 0;
+do{
+    echo "*";
+    $i++;
+}while($i<$num);
+?>
---
+++ sa4-7.php
@@ -0,0 +1,10 @@
+<?php
+$num =0;
+while(true){
+    echo "Hello!<br>";
+    $num++;
+    if($num === 4){
+        break;
+    }
+}
+?>
