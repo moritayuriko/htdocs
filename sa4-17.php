b3742ef568d040512947c61b29fab166a1ac41f0
moritayuriko <yuriko-morita@infinityloop.co.jp>
2024-05-28 12:10:46 +0900

    -4-17まで

--- sa4-17.php
+++ sa4-17.php
@@ -1,13 +1,39 @@
-<?php
-//連想配列
-$rail=["kyuu"=>"きゅう","kahuka"=>"かふか","jinn"=>"じん"];
-foreach ($rail as $key=>$value){
-    echo $key . ":" .  $value . "<br>";
-}
-echo "<br>";
+<!DOCTYPE html>
 
-$library=["angela","laurent","book"];
-foreach($library as $key => $value){
-    echo $key . ":" . $value . "<br>";
-}
-?>
+<head>
+    <meta charset="UTF-8">
+    <title>簡単なテーブルのサンプルt</title>
+</head>
+
+<body>
+    <h1>簡単なテーブルのサンプル</h1>
+    <table border="1" style="border-collapse:collapse;">
+        <tr>
+            <th>名前</th>
+            <th>性別</th>
+            <th>年齢</th>
+            <th>住所</th>
+        </tr>
+        <tr>
+            <td>穹</td>
+            <td>?</td>
+            <td>1</td>
+            <td>rail</td>
+        </tr>
+        <tr>
+            <td>カフカ</td>
+            <td>女</td>
+            <td>32</td>
+            <td>elio</td>
+        </tr>
+        <tr>
+            <td>刃</td>
+            <td>男</td>
+            <td>400</td>
+            <td>elio</td>
+        </tr>
+    </table>
+
+</body>
+
+</html>
